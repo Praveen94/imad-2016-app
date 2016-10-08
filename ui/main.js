bb91d7f44851b168d2e468434142cafbe49449cb
Praveen94 <pr5994@yahoo.com>
2016-10-08 09:35:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,8 @@
 console.log('Loaded!');
 var img=document.getElementById('madi');
-var i=0;
-img.onmouseenter=function() {
-    for(i=0;i<100;i++)
+function anim() {
     img.style.marginLeft='100px';
-    };
+    }
     
-//anim();
+img.onmouseenter=anim();
 
