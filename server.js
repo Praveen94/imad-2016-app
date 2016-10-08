6422c3c8b6e5f615ad3a884ac8c2e487f32a32db
Praveen94 <pr5994@yahoo.com>
2016-10-08 17:03:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-one',function(req,res){
 var counter=0;
 app.get('/counter',function(req,res){
 	counter=counter+1;
-	res.send((counter.toString()))});
+	res.send((counter.toString()))}); //only string response
 
 app.get('/article-two',function(req,res){
 	res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
