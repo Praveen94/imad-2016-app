e68354def0fb45180534fd05cbdd88414bc6c2f7
Praveen94 <pr5994@yahoo.com>
2016-10-09 17:58:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,35 @@
 //console.log('Loaded!');
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
-    counter+=1;
-var span=document.getElementById('count');
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function()
+  {
+      if(request.readystate==XMLHttprequest.DONE)
+  {
+    if(request.status==200)
+    {
+      var counter= request.responseText;
+    var span=document.getElementById('count');
 span.innerHTML=counter.toString();
+        
+    }
+  }
+      
+  };
+  
+  
+  
+  
+  
+  
+//    counter+=1;
+
+request.open('GET','http://http:praveen94.imad.hasura-app.io/counter',true);    
+request.send(null);
     
 };
+
     
 
 
