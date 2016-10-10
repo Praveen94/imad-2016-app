8312e4259a3e4bfeff440f7b8a268dc034c6ee76
Praveen94 <pr5994@yahoo.com>
2016-10-10 13:58:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,13 @@
 //console.log('Loaded!');
 var button=document.getElementById('counter');
-button.onclick=function(){
-button.innerHTML="Liked";    
-};
+//button.onclick=function(){
+//button.innerHTML="Liked";    
+//};
 
 
 //submit name
 
-/*var submit=document.getElementById('submit-btn');
+var submit=document.getElementById('submit-btn');
 submit.onclick=function()
 {
 var nameInput=document.getElementById('name');
@@ -32,7 +32,7 @@ ul.innerHTML=list;
     }
   }
       
-  };*/
+  };
   
   
   
