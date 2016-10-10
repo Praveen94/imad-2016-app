e3aa3809ea33e74665c62ee238eba3effb1b994e
Praveen94 <pr5994@yahoo.com>
2016-10-10 10:25:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,58 @@
+//console.log('Loaded!');
+var button=document.getElementById('counter');
+
+
+//submit name
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit-btn');
+submit.onclick=function()
+{
+ var request=new XMLHttpRequest();
+  request.onreadystatechange=function()
+  {
+      if(request.readyState==XMLHttpRequest.DONE)
+  {
+    if(request.status===200)
+    {
+     var names=request.responseText;
+    names=JSON.parse(names);
+    var list='';
+for(var i=0;i<names.length;i++)
+{
+    list+="<li>"+names[i]+"</li>";
+}
+var ul=document.getElementById('namelist');
+ul.innerHTML=list;
+
+    }
+  }
+      
+  };
+  
+  
+  
+  
+  
+  
+//    counter+=1;
+
+request.open('GET','http://praveen94.imad.hasura-app.io/submit_name?name='+name,true);    
+request.send(null);
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
