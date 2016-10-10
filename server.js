d13ff4e07aa13461c50a7359a601e947f00b95f8
Praveen94 <pr5994@yahoo.com>
2016-10-10 09:52:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var names=[];
+app.get('/submit-name/:name ',function(req,res){
+    var name=req.params.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+    
+});
 app.get('/article-one',function(req,res){
 	res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
