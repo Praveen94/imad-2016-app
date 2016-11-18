e98b2dae13c52a742a2f7f64f96a9a1e8269a15b
Praveen94 <pr5994@yahoo.com>
2016-11-18 09:09:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/hash/:input', function(req, res) {
    res.send(hashedString);
 });
 
-/*app.post('/create-user', function (req, res) {
+app.post('/create-user', function (req, res) {
    // username, password
    // {"username": "tanmai", "password": "password"}
    // JSON
@@ -150,25 +150,8 @@ app.post('/login', function (req, res) {
           }
       }
    });
-});*/
-
-app.post('/create-user',function(req,res)
-{
-var username=req.body.username;
-var password=req.body.password;
-var salt=crypto.randomBytes(128).toString();
-var dbString=hash(password,salt);
-    pool.query('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString],function(err,result)
-    {
-    if(err){
-        res.status(500).send(err.toString());
-    }
-    else
-     {
-         res.send("User Successfully created:"+username);
-     }
-    });    
 });
+
 
 
 
