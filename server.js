b69d90e7acdd4b7a86808218c835ad6f59ccd22e
Praveen94 <pr5994@yahoo.com>
2016-11-18 06:48:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -228,7 +228,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
         res.status(403).send('Only logged in users can comment');
     }
 });*/
-app.get('articles/:articleName',function(req,res)
+app.get('/articles/:articleName',function(req,res)
 {
 pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
     if (err) {
