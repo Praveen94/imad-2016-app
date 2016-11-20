5801d45304cca89867e4cf128f5c555a9be22f88
Praveen94 <pr5994@yahoo.com>
2016-11-20 18:21:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -167,13 +167,11 @@ function write_article()
        var request=new XMLHttpRequest();
        request.onreadystatechange=function()
        {
-        if(request.readyState==4)
+        if(request.readyState==4 && request.status==200)
         {
         submit.value='Posted';
         }
-        else{
-        alert("Post unsucessfull");
-            }
+       
         };
        var title=document.getElementById('art_title').value;    
        var date=document.getElementById('art_date').value;
