0e4dd14d422186ecb300a9898e9f4f97a5fb19f4
Praveen94 <pr5994@yahoo.com>
2016-11-25 17:47:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -96,7 +96,7 @@ function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     loginArea.innerHTML = `
         <h3> Hi <i>${username}</i></h3>
-        <a href="/logout"><input type="button">LogOut</input></a>
+        <a href="/logout"><button type="button">LogOut</button></a>
         
         <h3><center>You have Logged In!!!</center></h3>
     <img id="login_img" src="/ui/smile.png" />
