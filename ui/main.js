4b35e0a4c9e000737897428859d0972b42f46164
Praveen94 <pr5994@yahoo.com>
2016-11-25 16:45:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,7 @@ function loadLoginForm () {
               if (request.status === 200) {
                   submit.value = 'Sucess!';
               } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
+                  alert("Invalid credentials. Try again?");
               } else if (request.status === 500) {
                   alert('Something went wrong on the server');
                   submit.value = 'Login';
