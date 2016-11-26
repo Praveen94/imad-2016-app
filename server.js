bc203df1e7ec1f79631b69f23fffc3f3ace527ea
Praveen94 <pr5994@yahoo.com>
2016-11-26 19:49:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -153,7 +153,9 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send(`<html><body>Logged out!<br/><br/> <a href="#" class="btn btn-info btn-lg">
+   res.send(`<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script></head><body>Logged out!<br/><br/> <a href="#" class="btn btn-info btn-lg">
           <span class="glyphicon glyphicon-home"></span> Home
         </a></body></html>`);
 });
