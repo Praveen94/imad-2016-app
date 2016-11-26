efb0c6aef235a5d979834dcb7fc9032ef9c1ba94
Praveen94 <pr5994@yahoo.com>
2016-11-26 12:40:24 +0530

    [imad-console] Updates ui/welcome.js

--- ui/welcome.js
+++ ui/welcome.js
@@ -1,1 +1,18 @@
+var myString="Hi Everyone.I am Praveen Ramkumar.This is my web app.";
+var myArray=myString.split("");
+var loopTimer;
+function frameLooper()
+{
 
+if(myArray.length>0)
+{
+document.getElementById("myTypingText").innerHTML+=myArray.shift();
+}
+else{
+clearTimeout(loopTimer);
+}
+loopTimer=setTimeout('frameLooper()',100);
+
+
+}
+frameLooper();
