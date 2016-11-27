4102071800b2590a08600eaf44b0e9617ac948dc
Praveen94 <pr5994@yahoo.com>
2016-11-27 10:42:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,10 +52,8 @@ function createTemplate (data) {
               <h3>
                   ${heading}
               </h3>
-              <p>
-              by ${user_name}
-              </p>
-              <div>
+              
+             <div>
                   ${date.toDateString()}
               </div>
               <br /><br />
@@ -275,7 +273,7 @@ app.post('/submit_article',function(req,res){
     var date=req.body.date;
     var heading=req.body.heading;
     var content=req.body.content;
-    pool.query('INSERT INTO article(title,heading,date,content) VALUES($1,$2,$3,$4,$5)',[title,heading,date,content],function(err,result){
+    pool.query('INSERT INTO article(title,heading,date,content) VALUES($1,$2,$3,$4)',[title,heading,date,content],function(err,result){
     if(err)
     {
        res.status(500).send(err.toString());
